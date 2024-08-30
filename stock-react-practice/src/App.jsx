41fc1a1db16d485f35d63a5de22bb96e4e09f15e
antonclayton <anton.clayton@gmail.com>
2024-08-29 20:08:59 -0700

    tiny fix to env

--- stock-react-practice/src/App.jsx
+++ stock-react-practice/src/App.jsx
@@ -1,10 +1,36 @@
 import './App.css'
+import { fetchData } from './constants/api'
+import { useState, useEffect } from 'react'
 
 function App() {
 
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const result = await fetchData();
+        setData(result);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getData();
+  }, []) //occurs once on every reload of app.jsx
+
+
   return (
     <div className="stocks">
-      johnson
+      {data ? (
+        <div>{JSON.stringify(data, null, 2)}</div> // Display fetched data as JSON
+      ) : (
+        <div>No data available: {error}</div>
+      )}
     </div>
   )
 }
