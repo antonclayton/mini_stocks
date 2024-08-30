7efc60ae8e12450b60b8e4ac85db5f260a57c6a6
antonclayton <anton.clayton@gmail.com>
2024-08-29 18:56:23 -0700

    Adds API and basic API handling

--- stock-react-practice/src/App.jsx
+++ stock-react-practice/src/App.jsx
@@ -1,10 +1,36 @@
 import './App.css'
+import { fetchData } from './constants/api'
+import { useState, useEffect } from 'react'
 
 function App() {
 
+  const [data, setData] = useState(null);               // contains data from API
+  const [error, setError] = useState(null);             // contains error 
+  const [loading, setLoading] = useState(false);        // true = API data loading | false = API data not loading
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const result = await fetchData();
+        setData(result);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getData(); 
+  }, []) //occurs once on every reload of app.jsx component
+
+
   return (
     <div className="stocks">
-      johnson
+      {data ? (
+        <div>{JSON.stringify(data, null, 2)}</div> // Display fetched data as JSON
+      ) : (
+        <div>No data available: {error}</div>
+      )}
     </div>
   )
 }
